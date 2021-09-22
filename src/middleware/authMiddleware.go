31a9fd51102920771d3021f74ef52a59522237d8
akmamun <almamunkhan04@gmail.com>
2021-09-22 16:36:56 +0600

    added middleware

---
+++ src/middleware/authMiddleware.go
@@ -0,0 +1,34 @@
+package middleware
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+
+	helper "go-jwt/src/helpers"
+)
+
+// Auth validates token and authorizes users
+func Authentication() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		clientToken := c.Request.Header.Get("token")
+		if clientToken == "" {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": fmt.Sprintf("No Authorization header provided")})
+			c.Abort()
+			return
+		}
+
+		claims, err := helper.ValidateToken(clientToken)
+		if err != "" {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err})
+			c.Abort()
+			return
+		}
+
+		c.Set("email", claims.Email)
+
+		c.Next()
+
+	}
+}
