fca14867eb49134e9a3b61108d28ed1bfa0c1530
akmamun <almamunkhan04@gmail.com>
2021-09-23 00:18:09 +0600

    fixed login reponse format

---
+++ src/server.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"net/http"
+	"os"
+
+	middleware "go-jwt/src/middleware"
+	routes "go-jwt/src/routes"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		port = "8000"
+	}
+
+	router := gin.New()
+	router.Use(gin.Logger())
+	routes.UserRoutes(router)
+
+	router.Use(middleware.Authentication())
+
+	router.GET("/", func(ctx *gin.Context) {
+
+		ctx.JSON(http.StatusOK, gin.H{"success": true, "data": nil, "message": "successfully login"})
+
+	})
+
+	router.Run(":" + port)
+}
